3b66692f6299601bf3bad2531c73dc41142259e8
James.Adams <James.Adams@CD0004059809.nesdis.noaa.local>
2017-11-02 17:19:50 -0400

    added reshape_to_divs_years_months() function

--- utils.py
+++ utils.py
@@ -48,3 +48,46 @@ def reshape_to_years_months(monthly_values):
     # reshape from (months) to (years, 12) in order to have one year of months per row
     return np.reshape(monthly_values, (total_years, 12))
             
+#-----------------------------------------------------------------------------------------------------------------------
+#@numba.jit
+def reshape_to_divs_years_months(monthly_values):
+    '''
+    :param monthly_values: an 2-D numpy.ndarray of monthly values, assumed to start at January of 
+                           the first year for each division
+    :return: the original monthly values reshaped to 3-D (divisions, years, 12), within each division each row 
+             representing a full year, with shape (years, 12)
+    :rtype: 3-D numpy.ndarray of floats
+    '''
+    
+    # if we've been passed a 3-D array with valid shape then let it pass through
+    shape = monthly_values.shape
+    if len(shape) == 3:
+        if shape[2] == 12:
+            # data is already in the shape we want, return it unaltered
+            return monthly_values
+        else:
+            message = 'Values array has an invalid shape (3-D but third dimension not 12): {}'.format(shape)
+            logger.error(message)
+            raise ValueError(message)
+    
+    # otherwise make sure that we've been passed in a 2-D array of values    
+    elif len(shape) != 2:
+        message = 'Values array has an invalid shape (not 2-D or 3-D): {}'.format(shape)
+        logger.error(message)
+        raise ValueError(message)
+
+    # pad the final months of the final year, if necessary
+    final_year_months = shape[1] % 12
+    if final_year_months > 0:
+        pad_months = 12 - final_year_months
+#         pad_values = np.full((shape[1], pad_months,), np.NaN)
+#         monthly_values = np.append(monthly_values, pad_values)
+        
+        monthly_values = np.pad(monthly_values, [(0, 0), (0, pad_months)], mode='constant', constant_values=np.NaN)
+        
+    # we should have an ordinal number of years now (ordinally divisible by 12)
+    total_years = int(monthly_values.shape[1] / 12)
+    
+    # reshape from (months) to (years, 12) in order to have one year of months per row
+    return np.reshape(monthly_values, (shape[0], total_years, 12))
+            
