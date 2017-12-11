3974dd4b4f697022edceeb24af39b9866a5f6a21
James Adams <monocongo@gmail.com>
2017-12-11 16:28:11 -0500

    clean up, moved tests into separate tests directory
    Former-commit-id: d08d6c564bdd6fee93e6dbb063bf4a05859209d3

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import logging
-import numba
 import numpy as np
 
 #-----------------------------------------------------------------------------------------------------------------------
