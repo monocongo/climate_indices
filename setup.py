8b5d77ac3ed547f3bde774b14e8ff3288a4f79c4
James.Adams <monocongo@gmail.com>
2017-12-26 20:16:41 -0500

    fleshed out the setuptools file with more details

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='indices_python',
-    version='0.1.0',
+    version='0.1',
     url='https://github.com/monocongo/indices_python',
     license='GPL 2.0',
     author='James Adams',
     author_email='james.adams@noaa.gov',
-    description='Community reference implementations of climate indices algorithms in Python. Including Palmers (PDSI, scPDSI,  PHDI, and Z-Index), SPI, SPEI, PET, and PNP.'
+    description='Community reference implementations of climate indices algorithms in Python. Including Palmers (PDSI, scPDSI,  PHDI, and Z-Index), SPI, SPEI, PET, and PNP.',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    scripts=['process_nclimdiv.py', 'process_nclimgrid.py'],
+    keywords="indices climate climate_indices drought drought_indices pdsi spi spei evapotranspiration",
 )
