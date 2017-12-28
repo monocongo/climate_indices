92c99cd3f3472333f48bacf1de27e3d9c909e105
James.Adams <James.Adams@CD0004059809.nesdis.noaa.local>
2017-12-27 23:14:14 -0500

    added some docstring, comments
    Former-commit-id: 04de0068b6a7dbc121d2f9c7c8b5d536ce35ed73

--- misc/nclimdiv_netcdf_to_ascii.py
+++ misc/nclimdiv_netcdf_to_ascii.py
@@ -13,19 +13,19 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 #-----------------------------------------------------------------------------
-def create_ascii_from_netcdf(input_netcdf, 
-                             output_ascii,
-                             states):
+def create_ascii(input_netcdf_filepath, 
+                 output_ascii_filepath,
+                 state_abbrevs):
     '''
     This function writes indicator values for all climate divisions to its respective ASCII files,
     which is formatted as input for the NCEI monitoring monthly indicator processor.
     
-    :param input_netcdf: input NetCDF containing indicator values
-    :param output_ascii: output ASCII file for soil constant values
-    :param states: 
+    :param input_netcdf_filepath: input NetCDF containing indicator values
+    :param output_ascii_filepath: output ASCII file for soil constant values
+    :param state_abbrevs: list of state abbreviations
     '''
     
-    with netCDF4.Dataset(input_netcdf, 'r') as input_dataset:
+    with netCDF4.Dataset(input_netcdf_filepath, 'r') as input_dataset:
         
         # get the initial and final year of the input datasets
         time_variable = input_dataset.variables['time']
@@ -35,28 +35,40 @@ def create_ascii_from_netcdf(input_netcdf,
         divisions = input_dataset.variables['division'][:]
         total_values = divisions.size * total_months
         
+        index_code_dict = {'spi_pearson_01': '71',
+                           'spi_pearson_02': '72',
+                           'spi_pearson_03': '73',
+                           'spi_pearson_06': '74',
+                           'spi_pearson_09': '75',
+                           'spi_pearson_12': '76',
+                           'spi_pearson_24': '77',
+                           'pdsi': '05',
+                           'phdi': '06',
+                           'zindex': '07'}
         for variable in input_dataset.variables:
 
-            if variable == 'spi_pearson_01':
-                code = '71'
-            elif variable == 'spi_pearson_02':
-                code = '72'
-            elif variable == 'spi_pearson_03':
-                code = '73'
-            elif variable == 'spi_pearson_06':
-                code = '74'
-            elif variable == 'spi_pearson_09':
-                code = '75'
-            elif variable == 'spi_pearson_12':
-                code = '76'
-            elif variable == 'spi_pearson_24':
-                code = '77'
-            elif variable == 'pdsi':
-                code = '05'
-            elif variable == 'phdi':
-                code = '06'
-            elif variable == 'zindex':
-                code = '07'
+            if variable in index_code_dict.keys():
+                index_code = index_code_dict[variable]
+#             if variable == 'spi_pearson_01':
+#                 index_code = '71'
+#             elif variable == 'spi_pearson_02':
+#                 index_code = '72'
+#             elif variable == 'spi_pearson_03':
+#                 index_code = '73'
+#             elif variable == 'spi_pearson_06':
+#                 index_code = '74'
+#             elif variable == 'spi_pearson_09':
+#                 index_code = '75'
+#             elif variable == 'spi_pearson_12':
+#                 index_code = '76'
+#             elif variable == 'spi_pearson_24':
+#                 index_code = '77'
+#             elif variable == 'pdsi':
+#                 index_code = '05'
+#             elif variable == 'phdi':
+#                 index_code = '06'
+#             elif variable == 'zindex':
+#                 index_code = '07'
             else:
                 continue
             
@@ -69,27 +81,33 @@ def create_ascii_from_netcdf(input_netcdf,
             data = np.ndarray.flatten(data)
     
             # UPDATE THIS IN PRODUCTION   
-            output_ascii = 'new_climdiv-{0}dv-v1.0.0-20161104'.format(variable)
+            output_ascii_filepath = 'new_climdiv-{0}dv-v1.0.0-20161104'.format(variable)
 
             # write the data as ASCII
-            _write_data_as_ascii(data, output_ascii, input_dataset.variables['division'][:], code, states, data_start_year, data_end_year)
+            _write_data_as_ascii(data, 
+                                 output_ascii_filepath, 
+                                 input_dataset.variables['division'][:], 
+                                 index_code, 
+                                 state_abbrevs, 
+                                 data_start_year)
     
 #-----------------------------------------------------------------------------------------------------------------------
 def _write_data_as_ascii(data, 
                          ascii_file,
                          divisions,
                          code,
-                         states,
+                         state_abbrevs,
                          start_year=1895):
     '''
     This function writes a time series of monthly data values, assumed to begin at January of the start year
     and end at December of the end year, as ASCII text.
     
-    :param data:
-    :param ascii_file:
-    :param divisions:
-    :param start_year:  
-    :param end_year:  
+    :param data: UD climate divisions data
+    :param ascii_file: output file to which this function will write ASCII values
+    :param divisions: US climate divisions
+    :param state_abbrevs: list of state abbreviations 
+    :param start_year: first year of the dataset
+    :param code: index code, such as PDSI  
     '''
     
     # reshape data into (divisions, years, months)
@@ -107,7 +125,7 @@ def _write_data_as_ascii(data,
             # write ID, year, etc.
             # fix the following line to conform to the current ASCII format (width of station ID, year, etc.)
             state_id_index = int(str(division_id)[-2:]) - 1
-            state = states[state_id_index]
+            state = state_abbrevs[state_id_index]
             padded_division_id = str(division_id).zfill(5)
             
             file.write('{0}{1}{2}{3}'.format(state, padded_division_id, code, str(start_year + year)))
@@ -124,15 +142,19 @@ def _write_data_as_ascii(data,
     file.close()
     
 #-----------------------------------------------------------------------------------------------------------------------
-def _parse_states(states_file):
-
-    df = pd.read_csv(states_file, 
+def _parse_states(states_csv):
+    """
+    Parse a file of states into a list of state abbreviations.
+    
+    :param states_csv: space separated rows with initial column containing state abbreviations 
+    """
+    df = pd.read_csv(states_csv, 
                      delim_whitespace=True, 
                      usecols=[0], 
                      names=['state_abbrev'])
-    states = df['state_abbrev'].tolist()
-    states = list(OrderedDict.fromkeys(states).keys())
-    return states
+    state_abbrevs = df['state_abbrev'].tolist()
+    state_abbrevs = list(OrderedDict.fromkeys(state_abbrevs).keys())
+    return state_abbrevs
     
 #-----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
@@ -147,11 +169,11 @@ if __name__ == '__main__':
         states = _parse_states(states_file)
         
         # call the function that creates ASCII files from data read from corresponding NetCDFs
-        create_ascii_from_netcdf(input_netcdf,
+        create_ascii(input_netcdf,
                                  output_ascii,
                                  states)
         
     except:
     
-        logger.exception('Failed to complete: {}'.format(sys.exc_info()))
+        logger.exception('Failed to complete: %s', sys.exc_info())
         raise
