173222dff28c2e203f2ad4b3c6176183c16da9c9
kikocorreoso <kikocorreoso@gmail.com>
2018-01-25 10:42:07 +0100

    working setup.py for PyPI (only when there is a table release

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='indices_python',
-    version='0.1',
+    version='0.1.0',
     url='https://github.com/monocongo/indices_python',
     license='GPL 2.0',
     author='James Adams',
     author_email='james.adams@noaa.gov',
-    description='Community reference implementations of climate indices algorithms in Python. Including Palmers (PDSI, scPDSI,  PHDI, and Z-Index), SPI, SPEI, PET, and PNP.',
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    description=('Community reference implementations of climate indices '
+                 'algorithms in Python. Including Palmers (PDSI, scPDSI,  '
+                 'PHDI, and Z-Index), SPI, SPEI, PET, and PNP.'),
+    packages=['indices_python'],
     install_requires=[
         "numpy",
         "numba",
@@ -17,5 +19,21 @@ setup(
         "scipy",
         "netcdf4",
     ],
-    keywords="indices climate climate_indices drought drought_indices pdsi spi spei evapotranspiration",
+    tests_require=[
+        "nose",
+    ],
+    test_suite='tests',
+    keywords=('indices climate climate_indices drought drought_indices pdsi '
+              'spi spei evapotranspiration'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
 )
