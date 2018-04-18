494698e3114a049fa1914315575ebf1180524b01
James.Adams <James.Adams@CD0004059809.nesdis.noaa.local>
2018-04-18 08:36:44 -0400

    #65 updated tests and fixtures, other cleanups performed while in there

--- tests/test_thornthwaite.py
+++ tests/test_thornthwaite.py
@@ -23,11 +23,13 @@ class ThornthwaiteTestCase(fixtures.FixturesTestCase):
                                                                  self.fixture_initial_data_year)
                                          
         # make sure that PET is being computed as expected
-        self.assertTrue(np.allclose(computed_pet, self.fixture_pet_mm), 
+        self.assertTrue(np.allclose(computed_pet, 
+                                    self.fixture_pet_mm.flatten(), 
+                                    equal_nan=True), 
                         'PET values not computed as expected')
         
         # make sure that a 3-D array raises an error
-        reshaped_temps = np.reshape(self.fixture_temps_celsius[0:1400], (14, 5, 20))
+        reshaped_temps = np.reshape(self.fixture_temps_celsius[0:1400], (123, 2, 6))
         self.assertRaises(ValueError, 
                           thornthwaite.potential_evapotranspiration, 
                           reshaped_temps, 
