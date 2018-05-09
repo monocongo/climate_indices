f02d9b0a165097659b6cedb3854fd4f4e25900d2
James Adams <monocongo@gmail.com>
2018-05-09 11:52:52 -0400

    #45 additional test coverage, clean up of fixtures, etc.

--- tests/test_thornthwaite.py
+++ tests/test_thornthwaite.py
@@ -20,7 +20,7 @@ class ThornthwaiteTestCase(fixtures.FixturesTestCase):
         # compute PET from the monthly temperatures, latitude, and initial years above
         computed_pet = thornthwaite.potential_evapotranspiration(self.fixture_temps_celsius,
                                                                  self.fixture_latitude_degrees, 
-                                                                 self.fixture_data_start_year)
+                                                                 self.fixture_data_year_start)
                                          
         # make sure that PET is being computed as expected
         self.assertTrue(np.allclose(computed_pet, 
@@ -34,21 +34,21 @@ class ThornthwaiteTestCase(fixtures.FixturesTestCase):
                           thornthwaite.potential_evapotranspiration, 
                           reshaped_temps, 
                           self.fixture_latitude_degrees, 
-                          self.fixture_data_start_year)
+                          self.fixture_data_year_start)
         
         # make sure that an invalid latitude value raises an error
         self.assertRaises(ValueError, 
                           thornthwaite.potential_evapotranspiration, 
                           self.fixture_temps_celsius, 
                           91.0,   # latitude > 90 is invalid 
-                          self.fixture_data_start_year)
+                          self.fixture_data_year_start)
 
         # make sure that an invalid latitude value raises an error
         self.assertRaises(ValueError, 
                           thornthwaite.potential_evapotranspiration, 
                           self.fixture_temps_celsius, 
                           -91.0,   # latitude < -90 is invalid 
-                          self.fixture_data_start_year)
+                          self.fixture_data_year_start)
 
 #-----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
