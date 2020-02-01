e95dc0958cbd6be8799677be5a68fd10c913bad9
James Adams <monocongo@gmail.com>
2020-02-01 09:42:28 -0500

    updated the setup.py and GitHub workflow to allow using any Python version >=3.6
    #360

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
-    python_requires=">=3",
+    python_requires=">=3.6",
     install_requires=[
         "dask",
         "nco",
