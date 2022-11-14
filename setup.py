7c97abcb7b6e460a08dbde60574607a9828dfc31
James Adams <monocongo@gmail.com>
2022-11-13 19:59:05 -0500

    [#470] most attributes etc. moved from setup.py into pyproject.toml

--- setup.py
+++ setup.py
@@ -1,62 +1,70 @@
-import pathlib
-from setuptools import find_packages, setup
+#!/usr/bin/env python
 
-# the directory containing this file
-BASE_DIR = pathlib.Path(__file__).parent
+from setuptools import setup
 
-# the text of the README file
-README = (BASE_DIR / "README.md").read_text()
+if __name__ == "__main__":
+    setup()
 
-setup(
-    name="climate_indices",
-    version="1.0.10",
-    url="https://github.com/monocongo/climate_indices",
-    license="BSD",
-    author="James Adams",
-    author_email="monocongo@gmail.com",
-    description=(
-        "Community reference implementations of climate index "
-        "algorithms in Python. Including Palmers (PDSI, scPDSI,  "
-        "PHDI, and Z-Index), SPI, SPEI, PET, and PNP."
-    ),
-    long_description=README,
-    long_description_content_type="text/markdown",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    include_package_data=True,
-    python_requires=">=3.6",
-    install_requires=[
-        "dask",
-        "nco",
-        "netcdf4",
-        "numba",
-        "numpy",
-        "scipy",
-        "toolz",
-        "xarray",
-    ],
-    keywords=[
-        "indices", "climate", "climate indices", "drought",
-        "drought indices", "pdsi ", "spi", "spei", "evapotranspiration",
-    ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        'License :: OSI Approved :: BSD License',
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Scientific/Engineering :: Atmospheric Science",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
-    entry_points={
-        "console_scripts": [
-            "process_climate_indices=climate_indices.__main__:main",
-            "spi=climate_indices.__spi__:main",
-        ]
-    },
-)
+
+# import pathlib
+# from setuptools import find_packages, setup
+#
+# # the directory containing this file
+# BASE_DIR = pathlib.Path(__file__).parent
+#
+# # the text of the README file
+# README = (BASE_DIR / "README.md").read_text()
+#
+# setup(
+#     name="climate_indices",
+#     version="1.0.10",
+#     url="https://github.com/monocongo/climate_indices",
+#     license="BSD",
+#     author="James Adams",
+#     author_email="monocongo@gmail.com",
+#     description=(
+#         "Community reference implementations of climate index "
+#         "algorithms in Python. Including Palmers (PDSI, scPDSI,  "
+#         "PHDI, and Z-Index), SPI, SPEI, PET, and PNP."
+#     ),
+#     long_description=README,
+#     long_description_content_type="text/markdown",
+#     package_dir={'': 'src'},
+#     packages=find_packages(where='src'),
+#     include_package_data=True,
+#     python_requires=">=3.6",
+#     install_requires=[
+#         "dask",
+#         "nco",
+#         "netcdf4",
+#         "numba",
+#         "numpy",
+#         "scipy",
+#         "toolz",
+#         "xarray",
+#     ],
+#     keywords=[
+#         "indices", "climate", "climate indices", "drought",
+#         "drought indices", "pdsi ", "spi", "spei", "evapotranspiration",
+#     ],
+#     classifiers=[
+#         "Development Status :: 5 - Production/Stable",
+#         "Intended Audience :: Developers",
+#         "Intended Audience :: Education",
+#         "Intended Audience :: Science/Research",
+#         'License :: OSI Approved :: BSD License',
+#         "Programming Language :: Python :: 3.6",
+#         "Programming Language :: Python :: 3.7",
+#         "Programming Language :: Python :: 3.8",
+#         "Programming Language :: Python :: 3.9",
+#         "Topic :: Software Development :: Libraries :: Python Modules",
+#         "Topic :: Scientific/Engineering :: Atmospheric Science",
+#         "Topic :: Scientific/Engineering :: Physics",
+#     ],
+#     entry_points={
+#         "console_scripts": [
+#             "process_climate_indices=climate_indices.__main__:main",
+#             "spi=climate_indices.__spi__:main",
+#         ]
+#     },
+# )
